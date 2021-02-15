0f2c3356fe1a649902fd4a596b336c0848ca3e1a
Anuj Sharma <anujsharma6299@gmail.com>
2021-02-15 20:16:27 +0530

    xmeme

--- backend/install.sh
+++
@@ -1,4 +0,0 @@
-sudo apt update
-sudo apt-get install -y python3-pip
-sudo apt-get install -y python3-django
-pip install djangorestframework
--- backend/server_run.sh
+++
@@ -1,7 +0,0 @@
-cd src/backend
-
-pip install django
-pip install djangorestframework
-pip install gunicorn
-pip install django-heroku
-pip install django-crispy-forms
